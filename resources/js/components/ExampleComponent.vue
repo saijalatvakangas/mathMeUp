fd6175b91eef7905ba9c45d6102fdca8dd8d5b59
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-09-08 21:01:36 +0300

    add Vuetify

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+  <v-app>
+    <v-card class="ma-auto" max-width="400">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title>Math Me Up!</v-card-title>
+      </v-img>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+      <v-card-text class="text--primary">
+        <div>Number on game!</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="orange" text>Share</v-btn>
+        <v-btn color="orange" text>Explore</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
 </script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,15 @@
 const mix = require('laravel-mix');
 
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
+let webpackConfig = {
+    plugins: [
+        new VuetifyLoaderPlugin()
+    ],
+}
+
+mix.webpackConfig( webpackConfig );
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
