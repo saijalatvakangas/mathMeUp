dc6ce1bfb2d4b62d18eb1948ccd0200a6a927cee
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-09-14 17:56:11 +0300

    add logic to retrieve and forward information

--- app/Http/Controllers/MathController.php
+++ app/Http/Controllers/MathController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Entities\AnswerOption;
+use App\Entities\Calculations\Addition;
+use App\Entities\Calculations\Calculation;
+use App\Entities\Calculations\Subtraction;
 use App\Entities\Question;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -11,29 +14,44 @@ class MathController extends Controller
 {
     public function createQuestion(Request $request): JsonResponse
     {
-        $answerOption1 = new AnswerOption(7, true);
-        $answerOption2 = new AnswerOption(8, false);
-        $questionData = new Question('3 + 4', [$answerOption1, $answerOption2]);
+        $level = (int)$request->get('level');
+        $questionData = $this->chooseCalculation()->createCalculation($level);
+
         return response()->json([
             'question'=>$questionData->getQuestion(),
-            'answerOptions'=>[
-                [
-                    'number' => 30,
-                    'isCorrect' => false
-                ],
-                [
-                    'number' => 50,
-                    'isCorrect' => false
-                ],
-                [
-                    'number' => 100,
-                    'isCorrect' => false
-                ],
-                [
-                    'number' => 10,
-                    'isCorrect' => true
-                ]
-            ]
+            'answerOptions'=> $this->formAnswerOptionJson($questionData->getAnswerOptions()),
+            'level'=> $request->get('level')
         ]);
+    }
+
+
+    protected function chooseCalculation(): Calculation
+    {
+        $option = random_int(1,2);
+        if ($option === 1) {
+            return new Addition();
+        }
+        return new Subtraction();
+    }
+
+    /**
+     * @param array|AnswerOption $answerOptions
+     * @return array
+     */
+    protected function formAnswerOptionJson(array $answerOptions): array
+    {
+        $answerArray = [];
+
+        /**
+         * @var AnswerOption $answerOption
+         */
+        foreach ($answerOptions as $answerOption) {
+            $answerArray[] =
+            [
+                'number' => $answerOption->getNumber(),
+                'isCorrect' => $answerOption->isCorrect()
+            ];
+        }
+        return $answerArray;
     }
 }
