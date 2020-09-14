1405f8cb480935218bd2f44f084f5f23db8bbca3
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-09-14 17:51:49 +0300

    add Subtraction class

---
+++ app/Entities/Calculations/Subtraction.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Entities\Calculations;
+
+use App\Entities\Question;
+
+class Subtraction extends BaseCalculation implements Calculation
+{
+    /**
+     * @param int $level
+     * @return Question
+     * @throws Exception
+     */
+    public function createCalculation(int $level): Question
+    {
+        $operation = $this->createSpecificTypeOfCalculation($level, '-');
+
+        return new Question($operation[self::KEY_OPERATION], $this->createAnswerOptions($operation));
+    }
+}
